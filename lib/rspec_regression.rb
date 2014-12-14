d323c9f90b9422cc2e7da8f810e3873cbadb7079
Willian van der Velde <mail@willian.io>
2014-12-14 18:16:36 +0100

    Total number of queries

--- lib/rspec_regression.rb
+++ lib/rspec_regression.rb
@@ -3,6 +3,8 @@ require 'awesome_print'
 require 'active_support'
 require "rspec_regression/version"
 
+RSpec::Support.require_rspec_core "formatters/base_text_formatter"
+
 # TODO: Fix the inconsistend use of :sqls and 'sqls'
 
 module RspecRegression
@@ -117,6 +119,18 @@ module RspecRegression
 
       ap analyser.diff_per_example
 
+      difference_in_number_of_queries = analyser.difference_in_number_of_queries
+
+      output, status = if difference_in_number_of_queries == 0
+                          ['Number of queries is stable!', :success]
+                        elsif difference_in_number_of_queries > 0
+                          ['Number of queries is increased!', :failure]
+                        elsif difference_in_number_of_queries < 0
+                          ['Number of queries is decreased!', :failure]
+                        end
+
+      puts "\nQuery regression: #{RSpec::Core::Formatters::ConsoleCodes.wrap(output, status)}"
+
     end
 
     private
@@ -133,13 +147,21 @@ module RspecRegression
   class Analyser
     def initialize(previous_results, current_results)
       @current_results = current_results
-      @previous_results = to_hash_with_name_as_key(previous_results)
+      @previous_results = previous_results
+      @previous_results_as_hash = to_hash_with_name_as_key(previous_results)
+    end
+
+    def difference_in_number_of_queries
+      current_number_of_queries = (@current_results.map { |example| example[:sqls].size }).inject :'+'
+      previous_number_of_queries = (@previous_results.map { |example| example['sqls'].size }).inject :'+'
+
+      current_number_of_queries - previous_number_of_queries
     end
 
     def diff_per_example
       [].tap do |d|
         @current_results.each do |current_example|
-          previous_example = @previous_results.fetch current_example[:name], {}
+          previous_example = @previous_results_as_hash.fetch current_example[:name], {}
 
           if (sqls_diff = diff_in_example previous_example, current_example)
             d << current_example.merge({ sqls: sqls_diff })
