f5aabdb262f5f64e3c6827253de845cfef6de21b
Willian van der Velde <@willian.io>
2015-11-07 12:32:41 +0100

    Spec update

--- dummy/spec/models/person_spec.rb
+++ dummy/spec/models/person_spec.rb
@@ -2,20 +2,21 @@ require 'rails_helper'
 
 RSpec.describe Person, type: :model do
   let(:person) { Person.create! name: 'Rein' }
+  let(:some_number) { ENV['SOME_NUMBER'].to_i }
 
   it 'has all the peoples' do
-    expect(Person.all).to eq [person]
+    some_number.times do
+      expect(Person.all).to eq [person]
+    end
   end
 
   context 'with variable peoples to create' do
-    let(:amount_of_peoples_to_create) { ENV['AMOUNT_OF_PEOPLES_TO_CREATE'].to_i }
-
     it 'creates some more peoples' do
-      amount_of_peoples_to_create.times do |i|
+      some_number.times do |i|
         Person.create! name: "Person #{i}"
       end
 
-      expect(Person.count).to eq amount_of_peoples_to_create
+      expect(Person.count).to eq some_number
     end
   end
 end
