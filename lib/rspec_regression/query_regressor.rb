f5d5514204a423e4d026122d5b0e4550f3b961b9
esjee <ewfawefw3@gmail.com>
2016-01-05 19:11:15 +0100

    fix bin file

--- lib/rspec_regression/query_regressor.rb
+++ lib/rspec_regression/query_regressor.rb
@@ -2,6 +2,63 @@ require 'json'
 require 'httparty'
 
 module RspecRegression
+  class RegressorConsoleShower
+    def initialize(left_tag, right_tag)
+      @results = results
+    end
+
+    def show
+      require 'awesome_print'
+      puts "\n\n## THE MIGHTY REGRESSOR"
+
+      unless results['results'].empty?
+        results['results'].each do |result|
+          puts "= Regression detected in: #{result['example_name']} (#{result['example_location']})"
+          result['queries_that_got_added'].each do |query|
+            puts " (+) #{query}"
+          end
+
+          result['queries_that_got_removed'].each do |query|
+            puts " (-) #{query}"
+          end
+        end
+      else
+        print 'No regressions detected, yeeh!'
+      end
+    end
+
+    private
+
+    attr_reader :left_tag, :right_tag
+
+    def results
+      @results ||= HTTParty.get regressor_url,
+        query: query_parameters,
+        headers: headers
+    end
+
+    def regressor_domain
+      ENV['REGRESSOR_DOMAIN']
+    end
+
+    def regressor_url
+      "#{regressor_domain}/api/results/compare_latest_of_tags.text"
+    end
+
+    def query_parameters
+      {
+        left_tag: left_tag,
+        right_tag: right_tag,
+      }
+    end
+
+    def headers
+      {
+        'AUTHORIZATION' => "Token token=\"#{regressor_api_token}\"",
+      }
+    end
+  end
+
   class QueryRegressor
     attr :current_example, :examples
 
@@ -57,63 +114,6 @@ module RspecRegression
       end
 
       @subscribed_to_notifications = true
-    end
-  end
-
-  class RegressorConsoleShower
-    def initialize(left_tag, right_tag)
-      @results = results
-    end
-
-    def show
-      require 'awesome_print'
-      puts "\n\n## THE MIGHTY REGRESSOR"
-
-      unless results['results'].empty?
-        results['results'].each do |result|
-          puts "= Regression detected in: #{result['example_name']} (#{result['example_location']})"
-          result['queries_that_got_added'].each do |query|
-            puts " (+) #{query}"
-          end
-
-          result['queries_that_got_removed'].each do |query|
-            puts " (-) #{query}"
-          end
-        end
-      else
-        print 'No regressions detected, yeeh!'
-      end
-    end
-
-    private
-
-    attr_reader :left_tag, :right_tag
-
-    def results
-      @results ||= HTTParty.get regressor_url,
-        query: query_parameters,
-        headers: headers
-    end
-
-    def regressor_domain
-      ENV['REGRESSOR_DOMAIN']
-    end
-
-    def regressor_url
-      "#{regressor_domain}/api/results/compare_latest_of_tags.text"
-    end
-
-    def query_parameters
-      {
-        left_tag: left_tag,
-        right_tag: right_tag,
-      }
-    end
-
-    def headers
-      {
-        'AUTHORIZATION' => "Token token=\"#{regressor_api_token}\"",
-      }
     end
   end
 
